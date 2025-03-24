94c99b76d71b54bfc2136b7b9d923a44bb116efb
eyelow78 <d.yagodarov2004@mail.ru>
2025-03-25 01:11:43 +0300

    add nordic-rose project

---
+++ nordic-rose/laravel/resources/views/nordicRose.blade.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href={{ asset('css/normalize.css')}}>
+    <link rel="stylesheet" href={{ asset('css/fonts.css')}}>
+    <link rel="stylesheet" href={{ asset('css/style.css')}}>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nordic Rose</title>
+</head>
+    <body class="body">
+        <header class="header">
+            <div class="header__logo">
+                <a class="logo__link" href="/">
+                    <img class="logo__img" src="img/logo.png" alt="Nordic Rose">
+                </a>
+            </div>
+            <nav class="header__nav-bar">
+                <ul class="nav-bar__list">
+                    <li class="nav-bar__item nav-bar__item--active">
+                        <a class="nav-bar__link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-bar__item">
+                        <a class="nav-bar__link" href="#">About</a>
+                    </li>
+                    <li class="nav-bar__item">
+                        <a class="nav-bar__link" href="#">Links</a>
+                    </li>
+                    <li class="nav-bar__item">
+                        <a class="nav-bar__link" href="#">Projects</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main class="main">
+            <div class="blog-intro">
+                <div class="blog-intro__image">
+                    <img src="img\main-image.png" alt="main-image">
+                </div>
+                <div class="blog-intro__title-container">
+                    <h2 class="blog-intro__title">A few words about this blog platform, Ghost, and how this site was made</h2>
+                </div>
+                <div class="blog-intro__description-container">
+                    <p class="blog-intro__description">Why Ghost (& Figma) instead of Medium, WordPress or other options?</p>
+                </div>
+            </div>
+            <div class="articles-list">
+                <div class="articles-list__title-container">
+                    <p class="articles-list__title">All articles</p>
+                </div>
+                <div class="articles-list__cards">
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-1.png" alt="article-1">
+                        </div>
+                        <div class="article-card__description">
+                            <p>Here are some things you should know regarding how we work</p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-2.png" alt="article-2">
+                        </div>
+                        <div class="article-card__description">
+                            <p>Granny gives everyone the finger, and other tips from OFFF Barcelona</p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-3.png" alt="article-3">
+                        </div>
+                        <div class="article-card__description">
+                            <p>Hello world, or, in other words, why this blog exists</p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-4.png" alt="article-4">
+                        </div>
+                        <div class="article-card__description">
+                            <p>Here are some things you should know regarding how we work </p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-5.png" alt="article-5">
+                        </div>
+                        <div class="article-card__description">
+                            <p>Connecting artificial intelligence with digital product design</p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-6.png" alt="article-6">
+                        </div>
+                        <div class="article-card__description">
+                            <p>It’s all about finding the perfect balance</p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-7.png" alt="article-7">
+                        </div>
+                        <div class="article-card__description">
+                            <p>I believe learning is the most important skill</p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-8.png" alt="article-8">
+                        </div>
+                        <div class="article-card__description">
+                            <p>Clients are part of the team</p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-9.png" alt="article-9">
+                        </div>
+                        <div class="article-card__description">
+                            <p>Clients are part of the team</p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-10.png" alt="article-10">
+                        </div>
+                        <div class="article-card__description">
+                            <p>Here are some things you should know regarding how we work </p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-11.png" alt="article-11">
+                        </div>
+                        <div class="article-card__description">
+                            <p>Connecting artificial intelligence with digital product design</p>
+                        </div>
+                    </div>
+                    <div class="cards__article-card">
+                        <div class="article-card__image">
+                            <img src="img\article-12.png" alt="article-12">
+                        </div>
+                        <div class="article-card__description">
+                            <p>How modern remote working tools get along with Old School Cowboy's methods</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
